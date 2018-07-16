ef100d3231b41293108b8487ebc51fe734169ee1
18JBark <34016305+18JBark@users.noreply.github.com>
2018-07-16 17:03:49 +1000

    Update 3 - 16/7/18 - 5:03 pm

--- I.T. SAT.py
+++ I.T. SAT.py
@@ -10,37 +10,81 @@ class LoginScreen:
         self.usern = StringVar()
         self.usern = Label(master, text="Username:")
         self.usern.pack()
+        self.usern.grid(row=1, column=0)
 
         self.user = StringVar()
-        self.user = Entry(master, option='name')
+        self.user = Entry(master)
         self.user.pack()
+        self.user.grid(row=1, column=1)
 
         self.passwo = StringVar()
         self.passwo = Label(master, text="Password:")
         self.passwo.pack()
+        self.passwo.grid(row=2, column=0)
 
         self.passw = StringVar()
-        self.passw = Entry(master, option='pass')
+        self.passw = Entry(master)
         self.passw.pack()
+        self.passwo.grid(row=2, column=1)
 
         self.login_button = Button(master, text="Login", command=self.Login)
         self.login_button.pack()
+        self.login_button.grid(row=3, column=0)
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+        self.close_button.grid(row=3, column=1)
+
+    def Login(self, user, passw):
+        users = open ("usernames.txt",'r')
+        for line in users:
+            if user in line:
+                for line in users:
+                    if passw in line:
+                        self.CreateMainScreen()
+
+    def CreateMainScreen(self):
+        self.master.withdraw()
+        self.newWindow = Toplevel(self.master)
+        Main = MainScreen(self.newWindow)
+
+
+
+class MainScreen:
+    def __init__(self, master):
+        self.master = master
+        master.title("Inventory System")
+
+        self.search = StringVar()
+        self.search = Label(master, text="Search:")
+        self.search.pack()
+
+        self.SearchFunc = StringVar()
+        self.SearchFunc = Entry(master, option='search')
+        self.SearchFunc.pack()
+
+        self.Search_button = Button(master, text="Search", command=self.SearchFunction())
+        self.Search_button.pack()
+
+        self.Stock_button = Button(master, text="Stock", command=self.Stock())
+        self.Stock_button.pack()
+
+        self.Logout_button = Button(master, text="Logout", command=self.CreateLoginScreen())
+        self.Logout_button.pack()
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
-    def Login(self, name):
-        users = open ("usernames.txt",'r')
-        for line in users:
-            if name in line:
-                for line in users:
-                    if name in line:
-                        self.CreateWindow()
 
-    def CreateWindow(self):
+    def CreateLoginScreen(self):
         self.master.withdraw()
         self.newWindow = Toplevel(self.master)
-        Main = MainScreen(self.newWindow)
+        Log = LoginScreen(self.newWindow)
+
+    def CreateSearchScreen(self):
+        self.master.withdraw()
+        self.newWindow = Toplevel(self.master)
+        Log = SearchScreen(self.newWindow)
 
     def SearchFunction(name, self):
         print('Search function')
@@ -49,7 +93,6 @@ class LoginScreen:
                 if name in line:
                     print(line)
         return
-
 
     def Stock(self):
         file = open ("stock.txt",'a')
@@ -64,9 +107,19 @@ class LoginScreen:
         file.write('\n')
         return
 
-class MainScreen:
+
+class SearchScreen:
     def __init__(self, master):
+        self.master = master
+        master.title("Inventory System")
+
+
+class InfoScreen:
+    def __init__(self, master):
+        self.master = master
+        master.title("Inventory System")
+
 
 root = Tk()
-#my_gui = Inventory_System(root)
+my_gui = LoginScreen(root)
 root.mainloop()
